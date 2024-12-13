6612fa28c21e4a506010281d9fa6ff2852d0d5d1
Trebulkins <trebinivan2005@gmail.com>
2024-12-13 16:16:04 +0700

    it just works 3

--- app/src/main/java/com/example/mobile_lab6/CrimeListFragment.kt
+++ app/src/main/java/com/example/mobile_lab6/CrimeListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mobile_lab6
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,9 +14,16 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.UUID
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
+
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    }
+    private var callbacks: Callbacks? = null
+
     private lateinit var crimeRecyclerView: RecyclerView
     private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
@@ -23,6 +31,11 @@ class CrimeListFragment : Fragment() {
             CrimeListViewModel by lazy {
                 ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
             }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,6 +61,11 @@ class CrimeListFragment : Fragment() {
                     updateUI(crimes)
                 }
             })
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
     }
 
     private fun updateUI(crimes: List<Crime>) {
@@ -77,7 +95,7 @@ class CrimeListFragment : Fragment() {
         }
 
         override fun onClick(v: View) {
-            Toast.makeText(context, "${crime.title} выбран!", Toast.LENGTH_SHORT).show()
+            callbacks?.onCrimeSelected(crime.id)
         }
 
     }
