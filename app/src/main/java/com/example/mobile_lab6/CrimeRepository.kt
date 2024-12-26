b0135bef00fd4ab659315bcbd2a0893ef3d88e20
Trebulkins <trebinivan2005@gmail.com>
2024-12-26 21:24:36 +0700

    migration

--- app/src/main/java/com/example/mobile_lab6/CrimeRepository.kt
+++ app/src/main/java/com/example/mobile_lab6/CrimeRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.example.mobile_lab6.database.CrimeDatabase
+import com.example.mobile_lab6.database.migration_1_2
 import java.util.UUID
 import java.util.concurrent.Executors
 
@@ -16,7 +17,8 @@ constructor(context: Context) {
         context.applicationContext,
         CrimeDatabase::class.java,
         DATABASE_NAME
-    ).build()
+    ).addMigrations(migration_1_2).build()
+
 
     private val crimeDao = database.crimeDao()
     private val executor = Executors.newSingleThreadExecutor()
